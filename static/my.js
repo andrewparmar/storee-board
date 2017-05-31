6c4df00067b3989118f722e9ac519452f048ba49
Andrew Parmar <andrew.parmar@gmail.com>
2017-05-30 17:50:25 -1000

    Video modal function with static link. DB link pull pending.

--- static/my.js
+++ static/my.js
@@ -7,21 +7,33 @@ $(document).ready(function(){
         $("#trailer-video-container").empty();
     });
 
+    $(".movie-tile").click(function () {
+        var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
+        // var sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
+        var sourceUrl = 'https://www.youtube.com/embed/9mOIxyRTY5I?autoplay=1&html5=1';
+        console.log(sourceUrl);     
+        $("#trailer-video-container").empty().append($("<iframe></iframe>", {
+          'id': 'trailer-video',
+          'type': 'text-html',
+          'src': sourceUrl,
+          'frameborder': 0
+        }));
+    });
+
     // $(".movie-tile").click(function () {
-    //     var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
-    //     var sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
-    //     $("#trailer-video-container").empty().append($("<iframe></iframe>", {
-    //       'id': 'trailer-video',
-    //       'type': 'text-html',
-    //       'src': sourceUrl,
-    //       console.log(sourceUrl)
-    //       'frameborder': 0
-    //     }));
+    //     // var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
+    //     // var sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
+    //     // var sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
+    //     var sourceUrl = 'https://www.youtube.com/embed/9mOIxyRTY5I?autoplay=1&html5=1';
+    //     $("#trailer-video-container").empty().append('<iframe width="560" height="315" \
+    //      src="https://www.youtube.com/embed/9mOIxyRTY5I" \
+    //      frameborder="0" allowfullscreen></iframe>');
     // });
 
-    $(".movie-tile").click(function () {
-         $(this).css("color","red");
-    });
+    // $(".movie-tile").click(function () {
+    //      $(this).css("color","red");
+    //      $("#trailer-video-container").empty().append("<strong>Hello</strong>");
+    // });
 
     $('.movie-tile').hide().first().show("fast", function showNext() {
         $(this).next("div").show("fast", showNext);
